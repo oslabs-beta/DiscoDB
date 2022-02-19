53594f5be6c458d4b53f2ade2d7067067f6638a5
Jackson Tong <jacksonktong@gmail.com>
2022-02-19 15:52:49 -0800

    Imported drawer template from MUI. Working on newNote button functionality

---
+++ disco-db-demo/components/sideBar.jsx
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { Button } from '@mui/material';
+
+const drawerWidth = 240;
+const noteList = ['Document', 'Document', 'Document'];
+
+export default function SideBar() {
+  // const [noteArray, setNewNote] = useState(noteList);
+//On load, get request to database
+//populate notelist array with all documents pertaining to user
+  function newNoteHandler(){
+    //make POST request to user/notes with object of {username: username, cratedAt: unix time}
+    //after post req, expect response of res.locals.data = {_id:id}
+    //poplate note array with a new icon with unique ID
+    const newNoteInfo = {
+      username: "username",
+      createdAt: Date.now()
+    }
+    const testURL = '/api/hello';
+    const devURL = '/user/notes';
+    fetch(testURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newNoteInfo),
+    })
+    .then(res => res.json())
+    .then((data) => {
+      const uniqId = data.data;
+      const newNote = <ListItem button id={uniqId}>
+        <ListItemText primary="Document"/>
+      </ListItem>
+      noteList.push(newNote)
+      console.log(noteList)
+    })
+    .catch((err) => {return console.log('Error', err)})
+    console.log('clicked on newnote')    
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Placeholder Title
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+        <Divider />
+        <Button variant="outlined" onClick={newNoteHandler}>New Note</Button>
+        <Divider />
+        <List>
+          {noteList.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
--- disco-db-demo/pages/about.js
+++ disco-db-demo/pages/about.js
@@ -1,7 +1,11 @@
+import SideBar from "../components/sideBar.jsx"
+
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <div><SideBar/></div>
+      <h2>Hello</h2>
     </div>
   )
 }
