e55f6207116fd542b5603af3cb78c77261de3d52
Young Min Lee <youngmineeh@Youngs-Air.attlocal.net>
2022-02-22 09:48:52 -0800

    added userController with getUserNotes, createAndGetNewNote, modifyAndGetNote, and deleteNote middlewares. Modified userRouter to utilize the new middlewares. Added path.resolve on dotenv import to fix URI issue

--- disco-db-demo/server/models/dbConnection.js
+++ disco-db-demo/server/models/dbConnection.js
@@ -1,4 +1,5 @@
-require('dotenv').config({path: '../../.env'});
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 const mongoose = require('mongoose');
 const URI = process.env.DB_CONNECTION_STRING;
 
--- disco-db-demo/server/server.js
+++ disco-db-demo/server/server.js
@@ -6,9 +6,11 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const db = require('./models/dbConnection.js');
+const cookieParser = require('cookie-parser');
 
 const authRouter = require('./routers/authRouter');
 const userRouter = require('./routers/userRouter');
+const { sendData } = require('next/dist/server/api-utils');
 
 
 app.prepare().then(() => {
@@ -18,22 +20,36 @@ app.prepare().then(() => {
   // validate this is necessary
   db.connection();
 
+  server.use(cookieParser());
+  server.use(express.json());
   server.use('/auth', authRouter);
   server.use('/user', userRouter);
-
-  server.get('/test', (req, res) => {
-    console.log('testing using postman');
-    return res.sendStatus(200);
-  })
 
   server.get('/', (req, res) => {
     console.log('this is to the root endpoint');
     return handle(req, res);
   });
 
-  server.all('*', (req, res) => {
-    console.log('this is a request to the server');
-    return handle(req, res);
+  // server.all('*', (req, res) => {
+  //   console.log('this is a request to the server');
+  //   return handle(req, res);
+  // });
+
+  // local error handler
+  server.use((req, res) => {
+    res.status(404).send('Not Found')
+  });
+
+  // global error handler
+  server.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
   });
 
   server.listen(port, (err) => {
