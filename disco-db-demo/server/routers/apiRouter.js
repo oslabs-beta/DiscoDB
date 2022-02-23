ef11e00394d7fab88e09a48ec2ca965e069848c7
jacksonktong <95325884+jacksonktong@users.noreply.github.com>
2022-02-22 20:29:12 -0800

    Update apiRouter.js
    Updated third param of createResponse to successful message.

--- disco-db-demo/server/routers/apiRouter.js
+++ disco-db-demo/server/routers/apiRouter.js
@@ -6,7 +6,7 @@ const { createResponse } = require('../models/responseModel');
 router.post('/weather', 
   weatherController.getWeather,
   (req, res) => {
-    return res.status(200).json(createResponse(true, 200, 'city not found', res.locals));
+    return res.status(200).json(createResponse(true, 200, 'API call successful', res.locals));
 });
 
 module.exports = router;
