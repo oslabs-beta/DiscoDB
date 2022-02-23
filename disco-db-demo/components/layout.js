6e1ed2665684cae4d8c1253e8703826011d96062
Eric Gomez <ergomez0201@gmail.com>
2022-02-22 16:32:53 -0800

    resolved merge conflicts. integrated sidebar to work with layout

--- disco-db-demo/components/layout.js
+++ disco-db-demo/components/layout.js
@@ -3,14 +3,30 @@ import Sidebar from './Sidebar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
+import React, { useEffect } from 'react';
 
 export default function Layout({ children }) {
+  //temporay storage for props.
+  const userNoteArr = [{_id: 'foobar', title: 'foo', content: 'bar'}, {_id: '123', title: 'foobar', content: 'I love pizza'}];
+  useEffect(() => {
+    fetch('/user')
+    .then((res) => {
+      //Iterate thru retrived data and create a copy of each object into state array.
+      console.log('this is in the layout useEffect: ', res.data);
+      res.data.forEach((ele) => {
+        const userNote = {...ele};
+        userNotesArr.push(userNote);
+      });
+    })
+    .catch((err) => console.log('Error in fetching data', err))
+  }, []);
+
   return (
     <>
       <Box sx={{ display: 'flex'}}>
         <CssBaseline />
           <Navbar />
-          <Sidebar />
+          <Sidebar usernotes={userNoteArr}/>
           <Box component="main" sx={{ flexGrow: 1, p: 3, textAlign:'left' }}>
             <Toolbar />
               {children}
--- disco-db-demo/pages/user/index.js
+++ disco-db-demo/pages/user/index.js
@@ -2,22 +2,13 @@ import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Weather from '../../components/Weather';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
 import Layout from '../../components/layout';
 
 export default function User() {
-  //temporay storage for props.
-  const userNoteArr = [{_id: 'foobar', title: 'foo', content: 'bar'}, {_id: '123', title: 'foobar', content: 'I love pizza'}];
-  useEffect(() => {
-    fetch('/user')
-    .then((res) => {
-      //Iterate thru retrived data and create a copy of each object into state array.
-      res.data.forEach((ele) => {
-        const userNote = {...ele};
-        userNotesArr.push(userNote);
-      });
-    })
-    .catch((err) => console.log('Error in fetching data', err))
-  }, []);
 
   return (
     <Box>
@@ -31,22 +22,8 @@ export default function User() {
 
 User.getLayout = function getLayout(user) {
   return (
-    <Layout usernotes={userNoteArr}>
+    <Layout>
       {user}
     </Layout>
-    
   )
 }
-
-{/* <Box sx={{ display: 'flex'}}>
-      <CssBaseline />
-        <Navbar />
-        <Sidebar usernotes={userNoteArr}/>
-        <Box component="main" sx={{ flexGrow: 1, p: 3, textAlign:'left' }}>
-          <Toolbar />
-            <Typography paragraph>
-              Hello User
-            </Typography>
-            <Weather />
-        </Box>
-      </Box> */}
